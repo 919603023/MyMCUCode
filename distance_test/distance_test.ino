1caa2bd16c6c8f0b1f8277ce0fb82ef12f4bfe41
WGB <919603023@qq.com>
2020-02-16 19:07:28 +0800

    修好了

--- distance_test/distance_test.ino
+++ distance_test/distance_test.ino
@@ -1,32 +1,31 @@
-
 #include <stdio.h>
 #include "dht11.h"
 #include "SoftwareSerial.h"
+
 #define DHT11PIN 2
 #include <Wire.h>
-
+#include <stdio.h>
 #include <stdlib.h>
 #include "LiquidCrystal_I2C.h"
 #define _SS_MAX_RX_BUFF 128 // RX buffer size
 #define LED_PIN 6
 #define RELAY_PIN 5
 #define LIGHT_PIN A2
-SoftwareSerial mySerial(3,4 );
-
+SoftwareSerial mySerial(4, 3);
 dht11 DHT11;
-unsigned long ThisDisplay = 2000, NextDisplay = 2000;
+int ledStatus = 1;
+int GSM_Init_Flag = 1;
+unsigned long ledOn = 2000, ledOff = 2000;
 //实例化一个对象并设置LCD1602设备地址，这里的地址是0x3F，一般是0x20，或者0x27
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 String comdata = "";
+
 char *NumBuf[] = {"0030", "0031", "0032", "0033", "0034", "0035", "0036", "0037", "0038", "0039"};
 char *RH_TM_Buf = "5EA64E3AFF1A";                               //中文  度为:  的编码
 char buff[] = "0891683110900805F011000D91688146255844F50008AA"; //短信中心的编码//后面加数据长度和数据即可
 
 //0891683110908705F0040D91688146255844F50008022011413452230873AF58834FE1606F
 char _16[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-
-int GSM_Init_Flag = 0;
-
 void setup()
 {
   Serial.begin(9600);
@@ -34,27 +33,43 @@ void setup()
   GSM_init();
   lcd.init();
   lcd.backlight();
-
   pinMode(RELAY_PIN, OUTPUT); //5引脚为继电器
   pinMode(LED_PIN, OUTPUT);   //6引脚为LED灯
-
 }
 void loop()
 {
-  /*****显示刷新********************/
-   LCD_Display();
-  /*********************结束********************/
-  /********软串口接收信息*************************/
-  if (MySerial_Save(1) >= 0)
-  { 
-    Serial.println(comdata); //转发串口信息
-    if (Serial_Find(comdata, "+CMT") == 0) // 收到短信
+  unsigned long nowtime = millis(); //获取当前的系统运行时间长度
+  if (ledStatus == 1)               //如果当前lled灯状态为高电平，则执行该程序
+  {
+    if (nowtime > ledOn) //检测系统运行时间长度是否到500ms
     {
-      if (Serial_Find(comdata, "73AF58834FE1606F") == 0) //收到  环境信息
-      {
+      ledOn = nowtime;         //记录当前时间长度，第一次为500ms,赋值给ledOn
+      ledOff = nowtime + 2000; //计算出下一次led灯变化的时刻，第一次运行程序时应该在1000ms时关灯
+      timerIsr();              //关掉led灯
+      ledStatus = 0;           //记录当前led灯状态，下一次使用
+    }
+  }
 
+  else
+  {
+    if (nowtime > ledOff)
+    {
+      ledOff = nowtime;
+      ledOn = nowtime + 2000;
+      timerIsr();
+      ledStatus = 1;
+    }
+  }
+  if (MySerial_Save(1) >= 0)
+  {
+    if (Serial_Find(comdata, "+CMT") == 0)
+    {
+      if (Serial_Find(comdata, "73AF58834FE1606F") == 0)
+      {
+        //组包
+        //
         char buf[300] = "";
-        Send_Sms(buf, Make_Sms(buf)); //发送短信
+        Send_Sms(buf, Make_Sms(buf));
       }
       else if (Serial_Find(comdata, "5F00706F") == 0) //开灯
       {
@@ -85,75 +100,59 @@ void loop()
     {
       //发送TCP数据包
     }
-   
-
-    comdata = ""; //清除缓冲区
+    Serial.println(comdata);
+    comdata = "";
   }
-  /*********结束***********************/
 
   if (MySerial_Save(2) >= 0)
   {
-    mySerial.println(comdata); //转发串口信息
-    if (Serial_Find(comdata, "send") == 0) //串口调试：发送SEND发送短信
-    {
-      
-                    //清空缓冲区
-        char buf[300] = "";
-        Send_Sms(buf, Make_Sms(buf)); //发送短信
-    }
-    
-      comdata = "";
-            
-  }
 
+    if (Serial_Find(comdata, "SEND") == 0)
+    {
+      char buf[300] = "";
+
+      Send_Sms(buf, Make_Sms(buf));
+    }
+    mySerial.println(comdata);
+    comdata = "";
+  }
 }
-void LCD_Display() //Lcd屏幕显示函数
+void timerIsr() //定时器中断处理函数
 {
-  unsigned long nowtime = millis();
 
-  if (nowtime > ThisDisplay) //检测系统运行时间长度是否到1000ms
+  int chk = DHT11.read(DHT11PIN);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  char buf[20] = "";
+  sprintf(buf, "%d%%/%d", DHT11.humidity, DHT11.temperature);
+  lcd.print(buf);
+  lcd.print((char)223);
+  lcd.print('C');
+  if (GSM_Init_Flag == 1)
   {
-    ThisDisplay = nowtime;        //记录当前时间长度，第一次为1000ms,赋值给ThisDisplay
-    NextDisplay = nowtime + 2000; //计算出下一次led灯变化的时刻，第一次运行程序时应该在1000ms时关灯
-
-    int chk = DHT11.read(DHT11PIN);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    char buf[20] = "";
-
-    sprintf(buf, "%d%%/%d", DHT11.humidity, DHT11.temperature);
-    lcd.print(buf);
-    lcd.print((char)223);
-    lcd.print('C');
-    if (GSM_Init_Flag == 1)
-    {
-      lcd.setCursor(14, 0);
-      lcd.print("OK");
-    }
-    lcd.setCursor(8, 1);
-    char Buf[10] = "";
-    Get_Light(Buf);
-    lcd.print(Buf);
+    lcd.setCursor(14, 0);
+    lcd.print("OK");
   }
+  lcd.setCursor(8, 1);
+  char Buf[10] = "";
+  Get_Light(Buf);
+  lcd.print(Buf);
 }
-void GSM_init() //GSM初始化函数
+void GSM_init()
 {
   while (Second_AT_Command("AT\n", "OK", 3000) != 0)
   {
     delay(100);
   }
   delay(10000);
-  //if (Serial_Init_Find("Ready", 200) > 0)
-    GSM_Init_Flag = 1;Second_AT_Command("AT+CPMS=\"SM\",\"SM\",\"SM\"\n", "OK", 3000);
-    
   Second_AT_Command("ATE0\n", "OK", 3000);
   Second_AT_Command("AT+CNMI=3,2,0,0,0\n", "OK", 3000);
+
   Second_AT_Command("AT+CMGF=0\n", "OK", 3000);
-  
-  delay(100);
- // Second_AT_Command("AT+CIPSTART=\"TCP\",\"129.211.89.48\",8000", "OK", 3000);
+  Second_AT_Command("AT+CPMS=\"SM\",\"SM\",\"SM\"\n", "OK", 3000);
+
 }
-int Second_AT_Command(char *AT, char *Back, unsigned int OutTime) //发送AT指令
+int Second_AT_Command(char *AT, char *Back, unsigned int OutTime)
 {
   mySerial.write(AT);
   if (Serial_Init_Find(Back, OutTime) < 0)
@@ -190,22 +189,25 @@ int Serial_Init_Find(char *Back, unsigned int OutTime)
   }
   return -1;
 }
-int Serial_Find(String comdata, char *Back) //在string中查找指定字符串
+int Serial_Find(String comdata, char *Back)
 {
   if (comdata.indexOf(Back) != -1)
   {
+
+    //Serial.write(Back);
     return 0;
   }
   return -1;
 }
 
-int MySerial_Save(int type) //串口数据保存1为软串口2为硬串口
+int MySerial_Save(int type)
 {
-  comdata = "";
   if (type == 1)
   {
-    
-      while (mySerial.available())
+    if (mySerial.available())
+
+    {
+      while (mySerial.available() > 0)
       {
         comdata += char(mySerial.read());
         delay(2);
@@ -214,10 +216,14 @@ int MySerial_Save(int type) //串口数据保存1为软串口2为硬串口
       {
         return 1;
       }
+    }
+    return -1;
   }
   else if (type == 2)
   {
-   
+    if (Serial.available())
+
+    {
       while (Serial.available() > 0)
       {
         comdata += char(Serial.read());
@@ -227,30 +233,27 @@ int MySerial_Save(int type) //串口数据保存1为软串口2为硬串口
       {
         return 1;
       }
-    
-   
-  }return -1 ;
+    }
+    return -1;
+  }
 }
-int Send_Sms(char *sms, int len) //发送短信函数
+int Send_Sms(char *sms, int len)
 {
-  
   char buf[12] = "";
   sprintf(buf, "AT+CMGS=%d\n", len);
-  Serial.println(sms);Serial.println(len);
   if (Second_AT_Command(buf, ">", 3000) == 0)
   {
-  
+    buf[strlen(buf)] = '\0';
     mySerial.write(sms);
     mySerial.write(0x1a);
   }
 }
 
-int Make_Sms(char *buf) //制作环境检测的短信包
+int Make_Sms(char *buf)
 {
-
   char TM[9] = "";
   char RH[9] = "";
-  if (DHT11.humidity / 10 == 0) //制作温湿度信息包
+  if (DHT11.humidity / 10 == 0)
   {
     sprintf(RH, "%s", NumBuf[DHT11.humidity % 10]);
   }
@@ -266,6 +269,7 @@ int Make_Sms(char *buf) //制作环境检测的短信包
   {
     sprintf(TM, "%s%s", NumBuf[DHT11.temperature / 10], NumBuf[DHT11.temperature % 10]);
   }
+
   char Buf[10] = "";int a=Get_Light(Buf);
   if (a == 1)
   {
@@ -293,9 +297,13 @@ int Make_Sms(char *buf) //制作环境检测的短信包
   sprintf(bufp, "%c%c", _16[tmp / 16], _16[tmp % 16]);
 
   sprintf(buf, "%s%s%s", buff, bufp, bufc);
+  Serial.println(tmp);
+  Serial.write(_16[tmp / 16]);
+  Serial.write(_16[tmp % 16]);
+
+  Serial.println((String(buf).length() - 18) / 2);
   return (strlen(buf) - 18) / 2;
 }
-
 int Get_Light(char *buf) //获取当前亮度信息，
 {
   if (analogRead(LIGHT_PIN) > 700)
